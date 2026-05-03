9f6081deb4e4a847281724a3e6788fc117cb2d5e
lakshmir22 <lakshmi2rai@gmail.com>
2026-05-03 20:38:24 +0530

    connected frontend to backend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://team-task-manager-production-40ad.up.railway.app/',
         changeOrigin: true
       }
     }
